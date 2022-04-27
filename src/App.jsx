61648e393fcc320c57b3b53a2f4ccb9cf2b09439
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-04-26 23:09:25 -0300

    chamada a api concluida com sucesso

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+
 export default function App() {
+  const [listas, setListas] = useState([])
+  
+  useEffect(() => {
+    const fetchListas = async () => {
+      const { data } = await axios.get('http://127.0.0.1:8000/list/');
+      setListas(data)
+      console.log(listas)
+    } 
+    
+    fetchListas()
+  }, [])
+  
   return (
     <div className="App">
-      <p>hello, world</p>
+      { listas.map(item => 
+        <p>{ item.name }</p>
+      ) }
     </div>
   );
 }

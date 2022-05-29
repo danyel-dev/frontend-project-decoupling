bde3002e1a5d2b1aa91c57cf4fe88e13ab4162d0
Carlos Daniel Sousa <danyelpinheiro154@hotmail.com>
2022-05-29 20:13:21 -0300

    pré-layout para adicionar tarefas pronto

--- src/components/ItemComponent.jsx
+++ src/components/ItemComponent.jsx
@@ -4,10 +4,9 @@ import '../styles/itemComponent.css'
 export default function ItemComponent({ item }) {
     return (
         <li className='item'>
-            <p>
-                {item.status? <input type="checkbox" checked />: <input type="checkbox" />}
-                { item.name }
-            </p>
+            {item.status? <input type="checkbox" checked />: <input type="checkbox" />}
+            
+            <p>{ item.name }</p>
         </li>
     );
 };
--- src/components/ListComponent.jsx
+++ src/components/ListComponent.jsx
@@ -6,16 +6,17 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 
+
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: 0,
-  boxShadow: 24,
-  p: 4,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: 0,
+    boxShadow: 24,
+    p: 4,
 };
 
 
@@ -39,6 +40,11 @@ export default function ListComponent({ list }) {
                 <Box sx={style}>
                     <h1 className="title-list">{ list.name }</h1>
                     
+                    <form method="POST" className="form-add-todo">
+                        <input type="text" placeholder="Digite aqui o nome da tarefa" />
+                        <button>Criar</button>
+                    </form>
+
                     <ul className="list-todos">
                         {list.item_set.map(item => 
                             <ItemComponent item={item} />    
